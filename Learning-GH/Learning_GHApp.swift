062833d93505f16805d08aa228d4628a2664dca6
Lucky Osunbiyi <osunbiyi.temitope1@gmail.com>
2021-06-12 17:56:50 -0400

    Initial Commit

---
+++ Learning-GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning-GH
+//
+//  Created by Lucky Osunbiyi on 2021-06-12.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
